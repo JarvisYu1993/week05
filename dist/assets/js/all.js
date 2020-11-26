df8ade53a44899175b3b368a6a2277012359ba96
Jarvis <t5138169@gmail.com>
2020-11-26 11:29:52 +0800

    first commit

---
+++ dist/assets/js/all.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.reply-btn').click(function (e) {
+    e.preventDefault();
+    $('.reply-content').toggleClass('active');
+  });
+  $('.assignment-btn').click(function (e) {
+    e.preventDefault();
+    $('.reply-content').removeClass('active');
+  });
+});
+//# sourceMappingURL=all.js.map
